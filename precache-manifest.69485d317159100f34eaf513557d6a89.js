393f31062584e62608327bc5d4ee3a788f82d370
AMAC71 <mcconnellxca@gmail.com>
2019-03-22 18:48:49 -0400

    Updates

---
+++ precache-manifest.69485d317159100f34eaf513557d6a89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4da99f283fdbfef8502",
+    "url": "/hangboard-timer/static/js/runtime~main.6b9dfb5a.js"
+  },
+  {
+    "revision": "93d7d0cf6ee751057f5a",
+    "url": "/hangboard-timer/static/js/main.27d5b772.chunk.js"
+  },
+  {
+    "revision": "d39184818682d2f92e51",
+    "url": "/hangboard-timer/static/js/2.a212973d.chunk.js"
+  },
+  {
+    "revision": "93d7d0cf6ee751057f5a",
+    "url": "/hangboard-timer/static/css/main.2137dadf.chunk.css"
+  },
+  {
+    "revision": "b3cdc470a59c52c40879832171bd4f47",
+    "url": "/hangboard-timer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.436200c582ae9d28c0d6b54041a3a7d8.js"
+  "/hangboard-timer/precache-manifest.69485d317159100f34eaf513557d6a89.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/hangboard-timer/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
